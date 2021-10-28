c5fae133f7a17120ff9b2a888e8da516c449481c
Aswathymanikandan <aswathy26797@gmail.com>
2021-10-28 11:14:12 +0530

    4th commit

--- src/topic2/Task.java
+++ src/topic2/Task.java
@@ -1,22 +1,23 @@
 package topic2;
 class Class1 {
-	int speed, time;
-	void distance(int s,int t) {
-		speed=s;
-		time=t;
-		System.out.println(speed*time);
+	static void distance(int s,int t) {
+		int d;
+		d=s*t;
+		System.out.println(d);
 		}
-	static int formula(int a,int b)
+	int formula(int a,int b)
 	{
-		System.out.println(a+c);
+		
+		return a+b;
 	}
 	
 }
 public class Task {
-
 	public static void main(String[] args) {
-		Class1 Obj = new Class1();
-		Obj.distance(50,2);
+		Class1 Obj=new Class1();
+		Obj.distance(5,6);
+		int c =Obj.formula(5,2);
+		System.out.println("a+b=" + c);
 	}
 
 }
