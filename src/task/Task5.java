17ee0b42f7624c6d62e1df3cbb5357f8eb7fe078
Aswathymanikandan <aswathy26797@gmail.com>
2021-11-09 11:20:20 +0530

    collection framework

--- src/task/Task5.java
+++ src/task/Task5.java
@@ -1,18 +1,18 @@
-	package task;
-
-
+package task;
 
 public class Task5 {
 
 	enum Currency {
-		INR("#","India"),USD("$","Usa"),EUR("@","Europe");
-		
-		String symbol,country;
-		Currency(String country,String symbol) {
-			this.country=country;
-			this.symbol=symbol;
+		INR("#", "India"), USD("$", "Usa"), EUR("@", "Europe");
+
+		String symbol, country;
+
+		Currency(String country, String symbol) {
+			this.country = country;
+			this.symbol = symbol;
 		}
-		public static void checkcountry(String name) {
+
+		 public static void checkcountry(String name) {
 			for(Currency value1 : Currency.values()) {
 				if(value1.country.contentEquals(name)) {
 					System.out.println(value1);
@@ -22,12 +22,7 @@ public class Task5 {
 				System.out.println("invalid symbol");
 				}
 			}
-	
-	public static void main (String[] args) {
-		
-		checkcountry("#");
-		
+		 }
+
 	}
 	}
-	}
-}
