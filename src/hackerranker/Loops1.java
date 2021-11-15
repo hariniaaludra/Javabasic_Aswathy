364c75182e158b48b652ce3228ac8da3f434a2e2
Aswathymanikandan <aswathy26797@gmail.com>
2021-11-15 14:45:45 +0530

    Serialization1

--- src/hackerranker/Loops1.java
+++ src/hackerranker/Loops1.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Loops1 {
 	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-       int N = in.nextInt();
+       int n = in.nextInt();
        for(int i = 1; i <=10; i++)
        {
-           System.out.println(N + " x "+i+" = "+i*N);
+           System.out.println(n + " x "+i+" = "+i*n);
        }
    }
 }
